145c49f36ba14b07c1ff22348d92d79d4a9af14d
llopez02 <llopez02@nmsu.edu>
2022-04-27 15:11:25 -0600

    update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const socket = require('socket.io');
 const port = 3000;
 var mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const cookieParser = require('cookie-parser')
+
+
 
 
 mongoose.connect('mongodb://localhost:27017/userdata', {useNewUrlParser: true}).catch(error => console.log("Something went wrong: " + error));
@@ -12,9 +16,16 @@ const server = app.listen(port, function() {
   console.log('App listening on port 3000!');
 });
 
+app.use(cookieParser());
+
+app.use(function(req, res, next){
+	console.log(req.cookies);
+	next();
+});
+
 app.use(express.static('serve'))
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 const io = socket(server);
 io.on('connection', function (sockett) {
@@ -31,7 +42,7 @@ io.on('connection', function (sockett) {
 		console.log('Sent turn to '+ id);
 	});
 
-  sockett.on('hit', function (data) {
+  	sockett.on('hit', function (data) {
 		sockett.broadcast.to(id).emit('sendhit', data);
 		console.log('Sent turn to '+ id);
 	});
@@ -52,6 +63,10 @@ app.post("/data", function(req, res){
     var newUser = new User(req.body.data);
 	
 	newUser.wins = 0;
+	newUser.password = bcrypt.hashSync(newUser.password, 10);
+	res.cookie('account', newUser.user);
 	newUser.save();
-	console.log("User added to database");
+	console.log("User " + newUser.user+" added to database");
+	res.redirect("/home.html");
 });
+
